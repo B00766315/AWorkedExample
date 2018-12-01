e65b430e432b23b39da8ee64df30597d8992a7ce
Elise <elisefleming93@hotmail.com>
2018-12-01 14:42:44 +0000

    Added changes

--- AWorkedExample/src/main/java/com/elise/MyUI.java
+++ AWorkedExample/src/main/java/com/elise/MyUI.java
@@ -2,15 +2,17 @@ package com.elise;
 
 import javax.servlet.annotation.WebServlet;
 
+import java.util.*;
+
 import com.vaadin.annotations.Theme;
+import java.sql.*;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Grid;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,20 +26,62 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        Connection connection = null;
+
+        String connectionString = "jdbc:sqlserver://classdatabase.database.windows.net:1433;" + 
+			  "database=classdatabase;" + 
+			  "user=elise@classdatabase;" + 
+			  "password=Password123;" + 
+			  "encrypt=true;" + 
+			  "trustServerCertificate=false;" + 
+			  "hostNameInCertificate=*.database.windows.net;" +
+			  "loginTimeout=30;";
+
         final VerticalLayout layout = new VerticalLayout();
         
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        try 
+        {
+            // Connect with JDBC driver to a database
+            connection = DriverManager.getConnection(connectionString);
+            ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM customerTable;");
+            // Convert the resultset that comes back into a List - we need a Java class to represent the data (Customer.java in this case)
+            List<Customer> customers = new ArrayList<Customer>();
+            // While there are more records in the resultset
+            while(rs.next())
+            {   
+                // Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields, note how I skip the id)
+                customers.add(new Customer(rs.getString("first_name"), 
+                            rs.getString("last_name"), 
+                            rs.getBoolean("paid"), 
+                            rs.getDouble("amount")));
+                        
+            }
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
+            // Add my component, grid is templated with Customer
+            Grid<Customer> myGrid = new Grid<>();
+            // Set the items (List)
+            myGrid.setItems(customers);
+            // Configure the order and the caption of the grid
+            myGrid.addColumn(Customer::getFirst_name).setCaption("Name");
+            myGrid.addColumn(Customer::getLast_name).setCaption("Surname");
+            myGrid.addColumn(Customer::getAmount).setCaption("Total Amount");
+            myGrid.addColumn(Customer::isPaid).setCaption("Paid");
+
+            
+            // Add the grid to the list
+            layout.addComponent(myGrid);
+
+        } 
+        catch (Exception e) 
+        {
+            // This will show an error message if something went wrong
+            layout.addComponent(new Label(e.getMessage()));
+        }
+
         setContent(layout);
+   
+
+
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
